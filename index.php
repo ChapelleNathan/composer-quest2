189bc8ae344771ab1917aefd15c3170d1c0c1f3f
Chapelle Nathan <nathanchapelle76@gmail.com>
2021-10-18 11:51:44 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+use CowSay\Cow;
+
+require_once 'vendor/autoload.php';
+
+$estelle = new Cow('Bonsoir');
+
+echo $estelle;
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'nathanchapelle/quest2',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gipetto/cowsay' => array(
+            'pretty_version' => '1.1.0',
+            'version' => '1.1.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gipetto/cowsay',
+            'aliases' => array(),
+            'reference' => '85218ee4c0d18cc8753e08af54a8740aac12860a',
+            'dev_requirement' => false,
+        ),
+        'nathanchapelle/quest2' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
